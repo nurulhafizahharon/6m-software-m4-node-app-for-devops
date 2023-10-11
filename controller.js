5858a345ae4037fc30f2ec871ec3362950b24e7a
nurulhafizahharon <nurulhafizahharon@outlook.com>
2023-10-11 21:42:35 +0800

    Edit controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,6 @@
-
 // This is the only function that will be called by the endpoint.
-function print(req, res){
-    res.send("Hello world! This is Hafizah.");
+function print(req, res) {
+  res.send("Hello I'm Hafizah! I've successfully deployed my code in heroku!");
 }
 
 module.exports = print;
